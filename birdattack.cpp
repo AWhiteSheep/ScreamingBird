4bb9d0bf08954cd06389e676638a57be493db6a2
aliseeperreault <pera3307@usherbrooke.ca>
2021-02-24 18:06:26 -0500

    birdattack
    j'ai mis tout mon code en commentaire, donc ça ne devrait pas changer grand chose! :)))

---
+++ birdattack.cpp
@@ -0,0 +1,32 @@
+/*
+#include "birdattack.h"
+#include "scene.h"
+
+
+BirdAttack::BirdAttack():
+    feu(new QGraphicsPixmapItem(QPixmap(":/images/fireball05.png")))
+{
+
+}
+
+BirdAttack::~BirdAttack()
+{
+
+}
+
+qreal BirdAttack::getX() const
+{
+    return xPos;
+}
+
+void BirdAttack::freezeInPlace()
+{
+    xAnimation->stop();
+}
+
+void BirdAttack::setX(qreal x)
+{
+
+}
+
+*/
--- birdattack.h
+++ birdattack.h
@@ -4,12 +4,35 @@
 #include <QObject>
 #include <QGraphicsPixmapItem>
 #include <QMediaPlayer>
+#include <QGraphicsItemGroup>
+#include <QPropertyAnimation>
+#include <QGraphicsScene>
+#include "birditem.h"
+#include <QDebug>
 
 class BirdAttack
 {
+    /*
+    Q_OBJECT
+    Q_PROPERTY(qreal x READ x WRITE setX )
+
+    const qreal VITESSE_BOULE = 90;    //valeur à ajuster
 public:
     BirdAttack();
     ~BirdAttack();
+
+    qreal getX() const;
+    void setX(qreal x);
+    void freezeInPlace();
+private:
+    bool collidesWithEnemy();
+    QGraphicsPixmapItem * feu;
+    QPropertyAnimation * xAnimation;
+    QMediaPlayer * feuMedia;
+
+    qreal yPos;
+    qreal xPos;
+    */
 };
 
 #endif // BIRDATTACK_H
